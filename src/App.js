71d9a9097e2fb26b247a042680f141533af4dab1
GeanluisGL <94375631+GeanluisGL@users.noreply.github.com>
2022-03-09 23:24:09 -0400

    adding title

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+   <h1>AAAAAAAAAA</h1>  
      </div>
   );
 }
 
+/*
+For create a 
+1- git init 
+2- git status
+3- git add .
+4- git commit -m "init 1"
+
+*/
 export default App;
